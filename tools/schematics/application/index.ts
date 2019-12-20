1b2482050146ae3ec6a22b4f242be92d644a1d36
YinChang <yincahng0626@gmail.com>
2019-12-20 17:05:37 +0800

    schematics ng-alain

--- tools/schematics/application/index.ts
+++ tools/schematics/application/index.ts
@@ -1,41 +1,39 @@
-import { chain, externalSchematic, Rule, mergeWith, apply, move, Tree, url, SchematicContext, HostTree } from '@angular-devkit/schematics';
-import { updateJsonInTree, getWorkspacePath, toFileName } from '@nrwl/workspace';
+import { chain, externalSchematic, Rule, mergeWith, apply, move, Tree, url, SchematicContext, HostTree,template } from '@angular-devkit/schematics';
+import { updateJsonInTree, getWorkspacePath, toFileName, readJsonInTree } from '@nrwl/workspace';
 import { NodePackageInstallTask } from '@angular-devkit/schematics/tasks';
 
 var appProjectRoot = "";
 var appProjectName = "";
-var schema:any;
 
-function removeOrginalFiles() {
-  return (host: Tree) => {
-    host.visit(file => {
 
-      if (file.startsWith(`/${appProjectRoot}/src/`)) {
-        host.delete(file);
-        console.log('delete file', file);
-      }
-    })
-  };
-}
-
-function addFilesToRoot() {
+function addSchematicFiles(): Rule {
   return chain([
+    host => {
+      host.visit(file => {
+        if (file.startsWith(`/apps/${appProjectRoot}/src/`)) {
+          host.delete(file);
+          console.log('delete file', file);
+        }
+      })
+    },
     mergeWith(
-      apply(url('./files'), [
-        move(appProjectRoot),
-      ]),
+      apply(url('./files/ng-alain'), [
+        move(`apps/${appProjectRoot}`)
+      ])
     )
   ]);
 }
 
-function doUpdateJsonInTree(){
+function updateProject(): Rule {
   return (host: Tree) => {
-      return updateJsonInTree(getWorkspacePath(host), json => {
-        json.projects[appProjectName]
+    return chain([
+      host=>{
+        updateJsonInTree(getWorkspacePath(host),json=>{
+          json.projects[appProjectName]
           .architect.build.options.assets =
           [
-            `apps/${schema.directory}/alain/src/favicon.ico`,
-            `apps/${schema.directory}/alain/src/assets`,
+            `apps/${appProjectRoot}/src/favicon.ico`,
+            `apps/${appProjectRoot}/src/assets`,
             {
               "glob": "**/*",
               "input": "./node_modules/@ant-design/icons-angular/src/inline-svg/",
@@ -45,7 +43,46 @@ function doUpdateJsonInTree(){
         json.projects[appProjectName]
           .architect.build.options.styles =
           [
-            `apps/${schema.directory}/alain/src/styles.less`,
+            `apps/${appProjectRoot}/src/styles.less`,
+            "node_modules/font-awesome/css/font-awesome.css",
+            "node_modules/primeng/resources/themes/nova-light/theme.css",
+            "node_modules/primeicons/primeicons.css",
+            "node_modules/primeng/resources/primeng.min.css"
+          ];          
+        json.projects[appProjectName]
+          .architect.build.options.scripts =
+          [
+            "node_modules/@antv/g2/build/g2.js",
+            "node_modules/@antv/data-set/dist/data-set.min.js",
+            "node_modules/@antv/g2-plugin-slider/dist/g2-plugin-slider.min.js",
+            "node_modules/ajv/dist/ajv.bundle.js",
+            "node_modules/qrious/dist/qrious.min.js"
+          ];
+        return json;          
+        });
+      } 
+    ]);
+  };
+}
+
+function doUpdateJsonInTree(){
+  return (host: Tree) => {
+      return updateJsonInTree(getWorkspacePath(host), json => {
+        json.projects[appProjectName]
+          .architect.build.options.assets =
+          [
+            `apps/${appProjectRoot}/src/favicon.ico`,
+            `apps/${appProjectRoot}/src/assets`,
+            {
+              "glob": "**/*",
+              "input": "./node_modules/@ant-design/icons-angular/src/inline-svg/",
+              "output": "/assets/"
+            }
+          ];
+        json.projects[appProjectName]
+          .architect.build.options.styles =
+          [
+            `apps/${appProjectRoot}/src/styles.less`,
             "node_modules/font-awesome/css/font-awesome.css",
             "node_modules/primeng/resources/themes/nova-light/theme.css",
             "node_modules/primeicons/primeicons.css",
@@ -73,13 +110,16 @@ function installPackages() {
 }
 
 export default function(schema: any): Rule {
-  this.schema=schema;
   return (host: Tree, context: SchematicContext) => {
+    const workspaceJson = readJsonInTree(host, getWorkspacePath(host));
+    
     var appDirectory = schema.directory
       ? `${toFileName(schema.directory)}/${toFileName(schema.name)}`
       : `${toFileName(schema.name)}`;
     appProjectName = appDirectory.replace(new RegExp('/', 'g'), '-');
-    appProjectRoot = `apps/${appDirectory}`;
+    appProjectRoot = workspaceJson.newProjectRoot
+    ? `${workspaceJson.newProjectRoot}/${schema.name}`
+    : schema.name;
     return chain([
       externalSchematic('@nrwl/angular', 'application', {
         e2eTestRunner:'none',
@@ -88,9 +128,8 @@ export default function(schema: any): Rule {
         routing: true,
         style: 'less'
       }),
-      removeOrginalFiles(),
-      addFilesToRoot(),
-      doUpdateJsonInTree(),
+      addSchematicFiles(),
+      updateProject(),
       //installPackages()
     ])(host, context);
   };
