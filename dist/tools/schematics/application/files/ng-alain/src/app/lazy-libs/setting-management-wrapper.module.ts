0c58aa6ed295158b6374c49b19e784c19f2ba1cc
YinChang <yincahng0626@gmail.com>
2019-12-21 11:39:06 +0800

    build by nx and version and publish by lerna

---
+++ dist/tools/schematics/application/files/ng-alain/src/app/lazy-libs/setting-management-wrapper.module.ts
@@ -0,0 +1,7 @@
+import { NgModule } from '@angular/core';
+import { SettingManagementNgAlainModule } from '@fs/setting-management/ng-alain'
+
+@NgModule({
+  imports: [SettingManagementNgAlainModule],
+})
+export class SettingManagementWrapperModule { }
