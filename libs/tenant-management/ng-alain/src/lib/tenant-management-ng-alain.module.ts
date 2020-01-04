937284303f6817859db572c54fec464b6c801277
YinChang <yincahng0626@gmail.com>
2020-01-04 18:09:29 +0800

    refactory (identity,tenant-management) : remove ng-alain routingModule and set replace component

--- libs/tenant-management/ng-alain/src/lib/tenant-management-ng-alain.module.ts
+++ libs/tenant-management/ng-alain/src/lib/tenant-management-ng-alain.module.ts
@@ -1,19 +1,23 @@
 import { TenantManagementModule } from '@fs/tenant-management';
-import { CoreModule } from '@abp/ng.core';
+import { CoreModule, AddReplaceableComponent } from '@abp/ng.core';
 import { NgModule } from '@angular/core';
 import { NgAlainBasicModule } from '@fs/ng-alain/basic';
-import { TenantManagementNgAlainRoutingModule } from './tenant-management-ng-alain-routing.module';
 import { TenantsComponent } from './components/tenants/tenants.component';
 import { FeatureManagementNgAlainModule } from '@fs/feature-management/ng-alain'
+import { Store } from '@ngxs/store';
 
 @NgModule({
     declarations: [TenantsComponent],
+    entryComponents: [TenantsComponent],
     imports: [
         CoreModule,
         NgAlainBasicModule,
-        TenantManagementNgAlainRoutingModule,
         TenantManagementModule,
         FeatureManagementNgAlainModule
     ]
 })
-export class TenantManagementNgAlainModule { }
+export class TenantManagementNgAlainModule {
+    constructor(private store: Store) {
+        store.dispatch(new AddReplaceableComponent({ component: TenantsComponent, key: 'TenantManagement.TenantsComponent' }));
+    }    
+ }
