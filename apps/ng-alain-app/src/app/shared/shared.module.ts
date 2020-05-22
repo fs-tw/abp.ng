df93c3947e5148827da89f5707efc5bc8c3e05bc
YinChang <yincahng0626@gmail.com>
2020-05-22 10:59:09 +0800

    remove primeng and update package.json

--- apps/ng-alain-app/src/app/shared/shared.module.ts
+++ apps/ng-alain-app/src/app/shared/shared.module.ts
@@ -3,7 +3,6 @@ import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 import { NgModule } from '@angular/core';
 import { ThemeBasicModule } from '@abp/ng.theme.basic';
 import { ThemeSharedModule } from '@abp/ng.theme.shared';
-import { TableModule } from 'primeng/table';
 import { NgAlainBasicModule } from '@fs/ng-alain/basic';
 
 @NgModule({
@@ -14,7 +13,6 @@ import { NgAlainBasicModule } from '@fs/ng-alain/basic';
         //ThemeBasicModule,
         //AlainSharedModule,
         NgAlainBasicModule,
-        TableModule,
         NgbDropdownModule
 
     ],
@@ -24,7 +22,6 @@ import { NgAlainBasicModule } from '@fs/ng-alain/basic';
         //ThemeBasicModule,
         //AlainSharedModule,
         NgAlainBasicModule,
-        TableModule,
         NgbDropdownModule
 
     ],
