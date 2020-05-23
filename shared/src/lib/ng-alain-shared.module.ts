5e982e4941ce108e7b655e201d86672c5b26ea80
YinChang <yincahng0626@gmail.com>
2020-05-23 20:26:01 +0800

    fix theme.core name

--- shared/src/lib/ng-alain-shared.module.ts
+++ shared/src/lib/ng-alain-shared.module.ts
@@ -5,7 +5,7 @@ import { default as ngLang } from '@angular/common/locales/zh';
 import { NZ_I18N, zh_TW as zorroLang } from 'ng-zorro-antd';
 import { DELON_LOCALE, zh_TW as delonLang } from '@delon/theme';
 import { registerLocaleData } from '@angular/common';
-import { ThemeCoreModule } from '@fs/theme-core';
+import { ThemeCoreModule } from '@fs/theme.core';
 
 @NgModule({
   imports: [
