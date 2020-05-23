151fd4bba4da763b40616348f1851f1bef0c54ca
YinChang <yincahng0626@gmail.com>
2020-05-23 16:30:48 +0800

    remove fs.core

--- ng-alain/src/lib/components/tenant-box/tenant-box.component.ts
+++ ng-alain/src/lib/components/tenant-box/tenant-box.component.ts
@@ -13,7 +13,7 @@ import { TenantBoxComponent as AbpTenantBoxComponent,Account} from '@abp/ng.acco
 })
 export class TenantBoxComponent 
   extends AbpTenantBoxComponent 
-  implements OnInit, Account.TenantBoxComponentInputs, Account.TenantBoxComponentOutputs {
+  implements Account.TenantBoxComponentInputs, Account.TenantBoxComponentOutputs {
   @Input()
   mainContentRef: TemplateRef<any>;
 
