cbb60b6da66c1f2d139bc3ee5bf8e6bf48d018fb
運承 鍾 <kaozyc@gmail.com>
2020-06-24 15:31:00 +0800

    update setting

--- ng-alain/src/lib/components/users/users.component.ts
+++ ng-alain/src/lib/components/users/users.component.ts
@@ -3,27 +3,17 @@ import { ConfirmationService } from '@abp/ng.theme.shared';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { Store } from '@ngxs/store';
-import { SettingManagementParameters } from '@fs/setting-management';
+
 @Component({
   selector: 'ng-alain-users',
   templateUrl: './users.component.html',
 })
 export class UsersComponent extends AbpUsersComponent implements OnInit {
-    parameters = new SettingManagementParameters;
     constructor(
         private _confirmationService: ConfirmationService,
         private _fb: FormBuilder,
         private _store: Store,
         private _identityService: IdentityService) {
         super(_confirmationService, _fb, _store,_identityService);
-    }
-
-    setting(visible, providerKey){
-      this.parameters = {
-        providerKey: providerKey,
-        providerName: 'U',
-        visible: visible,
-        routerName: null
-      };
     }
 }
