c8c9dd7d0d2e7f5b4a143e9a82ee896d55e0f0b9
YinChang <yincahng0626@gmail.com>
2020-07-04 12:16:39 +0800

    move package.json to packages module

--- dist/tools/schematics/application/files/ng-alain/src/app/lazy-libs/setting-management-wrapper.module.ts
+++
@@ -1,7 +0,0 @@
-import { NgModule } from '@angular/core';
-import { SettingManagementNgAlainModule } from '@fs/setting-management/ng-alain'
-
-@NgModule({
-  imports: [SettingManagementNgAlainModule],
-})
-export class SettingManagementWrapperModule { }
--- dist/tools/schematics/application/files/ng-alain/src/app/main/readme.md
+++
@@ -1,4 +0,0 @@
-這邊會留下的話，應該是lib的公版無法滿足，才會留在這為module,否則只有使用lib
-
-1.main.module 要指定layout,所以要載入layout
-
