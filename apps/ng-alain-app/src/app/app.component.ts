df96cfb3b4f21c62348430195706f3e3996c1cee
YinChang <yincahng0626@gmail.com>
2020-07-04 17:01:24 +0800

    fix dev-app

--- apps/ng-alain-app/src/app/app.component.ts
+++ apps/ng-alain-app/src/app/app.component.ts
@@ -7,7 +7,10 @@ import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
-  template: ` <router-outlet></router-outlet> `,
+  template: `
+    <abp-loader-bar></abp-loader-bar>
+    <router-outlet></router-outlet>
+  `,
 })
 export class AppComponent implements OnInit {
   constructor(
