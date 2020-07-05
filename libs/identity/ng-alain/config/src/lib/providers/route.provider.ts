0c8bd148eec3be2a510108b7f8c8845db90f6d55
YinChang <yincahng0626@gmail.com>
2020-07-05 14:44:07 +0800

    test route add extreproperty

--- libs/identity/ng-alain/config/src/lib/providers/route.provider.ts
+++ libs/identity/ng-alain/config/src/lib/providers/route.provider.ts
@@ -1,11 +1,20 @@
-import { RoutesService, eLayoutType } from '@abp/ng.core';
 import { APP_INITIALIZER } from '@angular/core';
+import { eIdentityRouteNames } from '@abp/ng.identity/config';
+import { WrapRoutesService } from '@fs/theme.core';
 
 export const ROUTE_PROVIDERS = [
-  { provide: APP_INITIALIZER, useFactory: configureRoutes, deps: [RoutesService], multi: true },
+  { provide: APP_INITIALIZER, useFactory: configureRoutes, deps: [WrapRoutesService], multi: true },
 ];
 
-function configureRoutes(routes: RoutesService) {
+function configureRoutes(_routes: WrapRoutesService) {
   return () => {
+    let routes = _routes.Proxy;
+    routes.patch(eIdentityRouteNames.Users, {
+      profile: {
+        title: 'users title',
+        doc: 'user doc',
+        nav: { routeName: 'user sub ' }
+      }
+    })
   };
 }
