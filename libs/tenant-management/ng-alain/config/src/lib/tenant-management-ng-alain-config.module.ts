21dffbffa9e43aa560b720e720bc5111d20407da
YinChang0626 <yinchang0626@gmail.com>
2020-09-17 14:11:17 +0800

    update tenant-management

--- libs/tenant-management/ng-alain/config/src/lib/tenant-management-ng-alain-config.module.ts
+++ libs/tenant-management/ng-alain/config/src/lib/tenant-management-ng-alain-config.module.ts
@@ -1,19 +1,14 @@
-import { NgModule, APP_INITIALIZER, ModuleWithProviders } from '@angular/core';
-import { TenantManagementConfigModule} from '@abp/ng.tenant-management/config'
-import { noop } from '@abp/ng.core';
+import { NgModule, ModuleWithProviders } from '@angular/core';
+import { TENANT_MANAGEMENT_ROUTE_PROVIDERS} from '@abp/ng.tenant-management/config'
 import { ROUTE_PROVIDERS } from './providers/route.provider';
 import { STYLES_PROVIDERS } from './providers/styles.provider';
 
-@NgModule({
-  imports: [
-    TenantManagementConfigModule.forRoot()
-  ]
-})
+@NgModule()
 export class TenantManagementNgAlainConfigModule {
   static forRoot(): ModuleWithProviders<TenantManagementNgAlainConfigModule> {
     return {
       ngModule: TenantManagementNgAlainConfigModule,
-      providers: [ROUTE_PROVIDERS,STYLES_PROVIDERS],
+      providers: [TENANT_MANAGEMENT_ROUTE_PROVIDERS,ROUTE_PROVIDERS,STYLES_PROVIDERS],
     };
   }
 }
--- libs/tenant-management/ng-alain/src/lib/tenant-management-ng-alain.module.ts
+++ libs/tenant-management/ng-alain/src/lib/tenant-management-ng-alain.module.ts
@@ -1,20 +1,21 @@
-import { TenantManagementWrapModule } from '@fs/tenant-management/wrap';
-import { AddReplaceableComponent, LazyModuleFactory, CoreModule } from '@abp/ng.core';
+import { TenantManagementModule } from '@abp/ng.tenant-management';
+import { LazyModuleFactory, CoreModule } from '@abp/ng.core';
 import { NgModule, CUSTOM_ELEMENTS_SCHEMA, ModuleWithProviders, NgModuleFactory } from '@angular/core';
 import { NgAlainBasicModule } from '@fs/ng-alain/basic';
 import { TenantsComponent } from './components/tenants/tenants.component';
 import { FeatureManagementNgAlainModule } from '@fs/feature-management/ng-alain';
-import { Store } from '@ngxs/store';
+import { NgxValidateCoreModule } from '@ngx-validate/core';
 
 @NgModule({
-  declarations: [TenantsComponent],
-  entryComponents: [TenantsComponent],
-  exports: [TenantsComponent],
   imports: [
     NgAlainBasicModule,
     FeatureManagementNgAlainModule,
-    TenantManagementWrapModule
-  ]
+    NgxValidateCoreModule,
+    TenantManagementModule
+  ],
+  exports: [TenantsComponent],
+  declarations: [TenantsComponent],
+  entryComponents: [TenantsComponent]
 })
 export class TenantManagementNgAlainModule {
   static forChild(): ModuleWithProviders<TenantManagementNgAlainModule> {
