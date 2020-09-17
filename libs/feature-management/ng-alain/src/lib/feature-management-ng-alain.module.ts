a639fda77594a5c4598f00f845b478c37f7e247c
YinChang0626 <yinchang0626@gmail.com>
2020-09-17 14:08:20 +0800

    update feature

--- libs/feature-management/ng-alain/src/lib/feature-management-ng-alain.module.ts
+++ libs/feature-management/ng-alain/src/lib/feature-management-ng-alain.module.ts
@@ -1,16 +1,16 @@
-import { FeatureManagementWrapModule } from '@fs/feature-management/wrap';
+import { FeatureManagementModule } from '@abp/ng.feature-management';
 import { CoreModule } from '@abp/ng.core';
 import { NgModule } from '@angular/core';
 import { NgAlainBasicModule } from '@fs/ng-alain/basic';
 import { FeatureManagementComponent } from './components/feature-management/feature-management.component';
 
 @NgModule({
-    declarations: [FeatureManagementComponent],
     imports: [
         CoreModule,
         NgAlainBasicModule,
-        FeatureManagementWrapModule
+        FeatureManagementModule
     ],
-    exports: [FeatureManagementComponent]
+    exports: [FeatureManagementComponent],
+    declarations: [FeatureManagementComponent]
 })
 export class FeatureManagementNgAlainModule { }
