a8e0750624f6c6b96ad22398b44e22d5a136ddb9
ChangYinShung <changyenshung@gmail.com>
2020-09-22 17:20:59 +0800

    feat (fs.account) :  fix error when enter  manage-profile

--- libs/tmpl/account/ng-alain/src/lib/components/manage-profile/manage-profile.component.ts
+++ libs/tmpl/account/ng-alain/src/lib/components/manage-profile/manage-profile.component.ts
@@ -1,13 +1,34 @@
-import { fadeIn } from '@abp/ng.theme.shared';
-import { transition, trigger, useAnimation } from '@angular/animations';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngxs/store';
+import { eAccountComponents } from '@abp/ng.account';
+import { GetProfile, ProfileState } from '@abp/ng.core';
 
 @Component({
     selector: 'ng-alain-manage-profile',
     templateUrl: './manage-profile.component.html',
     styleUrls: ['./manage-profile.component.less'],
-    animations: [trigger('fadeIn', [transition(':enter', useAnimation(fadeIn))])],
 })
-export class ManageProfileComponent {
+export class ManageProfileComponent implements OnInit {
     selectedTab = 0;
+    changePasswordKey = eAccountComponents.ChangePassword;
+
+    personalSettingsKey = eAccountComponents.PersonalSettings;
+
+    isProfileLoaded: boolean;
+
+    hideChangePasswordTab: boolean;
+     currentProfile;
+    constructor(private store: Store) { }
+
+
+    ngOnInit() {
+        this.store.dispatch(new GetProfile()).subscribe(() => {
+            this.isProfileLoaded = true;
+            this.currentProfile = this.store.selectSnapshot(ProfileState.getProfile);
+            if (this.store.selectSnapshot(ProfileState.getProfile).isExternal) {
+                this.hideChangePasswordTab = true;
+                this.selectedTab = 1;
+            }
+        });
+    }
 }
--- libs/tmpl/theme-ng-alain/src/lib/ng-alain.module.ts
+++ libs/tmpl/theme-ng-alain/src/lib/ng-alain.module.ts
@@ -110,25 +110,26 @@ import { NgxValidateCoreModule } from '@ngx-validate/core';
 
 @NgModule({
   imports: [
+    //TODO:Valdation text wull  display error(only key)
     NgxValidateCoreModule.forRoot({
-      targetSelector: '.form-group',
-      blueprints: {
-        creditCard: 'AbpValidation::ThisFieldIsNotAValidCreditCardNumber.',
-        email: 'AbpValidation::ThisFieldIsNotAValidEmailAddress.',
-        invalid: 'AbpValidation::ThisFieldIsNotValid.',
-        max: 'AbpValidation::ThisFieldMustBeBetween{0}And{1}[{{ min }},{{ max }}]',
-        maxlength:
-          'AbpValidation::ThisFieldMustBeAStringOrArrayTypeWithAMaximumLengthOf{0}[{{ requiredLength }}]',
-        min: 'AbpValidation::ThisFieldMustBeBetween{0}And{1}[{{ min }},{{ max }}]',
-        minlength:
-          'AbpValidation::ThisFieldMustBeAStringOrArrayTypeWithAMinimumLengthOf{0}[{{ requiredLength }}]',
-        ngbDate: 'AbpValidation::ThisFieldIsNotValid.',
-        passwordMismatch: 'AbpIdentity::Identity.PasswordConfirmationFailed',
-        range: 'AbpValidation::ThisFieldMustBeBetween{0}And{1}[{{ min }},{{ max }}]',
-        required: 'AbpValidation::ThisFieldIsRequired.',
-        url: 'AbpValidation::ThisFieldIsNotAValidFullyQualifiedHttpHttpsOrFtpUrl',
-      },
-      //errorTemplate: ValidationErrorComponent,
+      //targetSelector: '.form-group',
+      //blueprints: {
+      //   creditCard: 'AbpValidation::ThisFieldIsNotAValidCreditCardNumber.',
+      //   email: 'AbpValidation::ThisFieldIsNotAValidEmailAddress.',
+      //   invalid: 'AbpValidation::ThisFieldIsNotValid.',
+      //   max: 'AbpValidation::ThisFieldMustBeBetween{0}And{1}[{{ min }},{{ max }}]',
+      //   maxlength:
+      //     'AbpValidation::ThisFieldMustBeAStringOrArrayTypeWithAMaximumLengthOf{0}[{{ requiredLength }}]',
+      //   min: 'AbpValidation::ThisFieldMustBeBetween{0}And{1}[{{ min }},{{ max }}]',
+      //   minlength:
+      //     'AbpValidation::ThisFieldMustBeAStringOrArrayTypeWithAMinimumLengthOf{0}[{{ requiredLength }}]',
+      //   ngbDate: 'AbpValidation::ThisFieldIsNotValid.',
+      //   passwordMismatch: 'AbpIdentity::Identity.PasswordConfirmationFailed',
+      //   range: 'AbpValidation::ThisFieldMustBeBetween{0}And{1}[{{ min }},{{ max }}]',
+      //required: 'AbpValidation::ThisFieldIsRequired',
+      //   url: 'AbpValidation::ThisFieldIsNotAValidFullyQualifiedHttpHttpsOrFtpUrl',
+      //}
+      //errorTemplate:ValidationErrorComponent,
     }),
     //ThemeCoreModule.forRoot(),
     NgAlainSharedModule.forRoot(),
