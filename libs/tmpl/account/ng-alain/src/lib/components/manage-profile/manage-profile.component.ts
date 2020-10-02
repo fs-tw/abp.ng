fb7e00370bb517f9e885ada6b140d093ebb6002f
YinChang0626 <yinchang0626@gmail.com>
2020-10-02 16:14:02 +0800

    extend abpManagementPofile

--- libs/tmpl/account/ng-alain/src/lib/components/manage-profile/manage-profile.component.ts
+++ libs/tmpl/account/ng-alain/src/lib/components/manage-profile/manage-profile.component.ts
@@ -1,9 +1,8 @@
 import { fadeIn } from '@abp/ng.theme.shared';
 import { transition, trigger, useAnimation } from '@angular/animations';
-import { Component, OnInit } from '@angular/core';
-import { eAccountComponents } from '@abp/ng.account';
+import { Component } from '@angular/core';
+import { ManageProfileComponent as AbpManageProfileComponent } from '@abp/ng.account';
 import { Store } from '@ngxs/store';
-import { GetProfile ,ProfileState} from '@abp/ng.core';
 
 @Component({
     selector: 'ng-alain-manage-profile',
@@ -11,25 +10,9 @@ import { GetProfile ,ProfileState} from '@abp/ng.core';
     styleUrls: ['./manage-profile.component.less'],
     animations: [trigger('fadeIn', [transition(':enter', useAnimation(fadeIn))])],
 })
-export class ManageProfileComponent implements OnInit {
-    selectedTab = 0;
-    changePasswordKey = eAccountComponents.ChangePassword;
+export class ManageProfileComponent extends AbpManageProfileComponent {
 
-  personalSettingsKey = eAccountComponents.PersonalSettings;
-
-  isProfileLoaded: boolean;
-
-  hideChangePasswordTab: boolean;
-  constructor(private store: Store) {}
-
-  ngOnInit() {
-    this.store.dispatch(new GetProfile()).subscribe(() => {
-      this.isProfileLoaded = true;
-      if (this.store.selectSnapshot(ProfileState.getProfile).isExternal) {
-        this.hideChangePasswordTab = true;
-        this.selectedTab = 1;
-      }
-    });
-  }
-
+    constructor(private _store:Store){
+        super(_store)
+    }
 }
