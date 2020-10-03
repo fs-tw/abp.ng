83520638d56d440b58e34ff40fb16386d7ffa2dc
YinChang0626 <yinchang0626@gmail.com>
2020-10-03 12:55:11 +0800

    replace component by service not store and add styles for confirmation

--- libs/tmpl/theme-ng-alain/basic/src/lib/providers/styles.provider.ts
+++ libs/tmpl/theme-ng-alain/basic/src/lib/providers/styles.provider.ts
@@ -1,46 +1,46 @@
-import { AddReplaceableComponent, CONTENT_STRATEGY, DomInsertionService } from '@abp/ng.core';
+import { CONTENT_STRATEGY, DomInsertionService, ReplaceableComponentsService } from '@abp/ng.core';
 import { APP_INITIALIZER } from '@angular/core';
-import { Store } from '@ngxs/store';
 import { LayoutPassportComponent, PageBarComponent } from '@fs/theme.ng-alain/layout';
 import { LayoutDefaultComponent } from '@fs/theme.ng-alain/layout';
 import { LayoutFullScreenComponent } from '@fs/theme.ng-alain/layout';
 import { eThemeBasicComponents } from '@abp/ng.theme.basic';
 import { eThemeCoreComponents } from '@fs/theme.core';
+import styles from '../constants/styles';
 
 export const BASIC_THEME_STYLES_PROVIDERS = [
   {
     provide: APP_INITIALIZER,
     useFactory: configureStyles,
-    deps: [Store],
+    deps: [DomInsertionService, ReplaceableComponentsService],
     multi: true,
   },
 ];
 
-export function configureStyles(store: Store) {
+export function configureStyles(
+  domInsertion: DomInsertionService,
+  replaceableComponents: ReplaceableComponentsService,
+) {
   return () => {
-    //domInsertion.insertContent(CONTENT_STRATEGY.AppendStyleToHead(styles));
-
-    initLayouts(store);
+    domInsertion.insertContent(CONTENT_STRATEGY.AppendStyleToHead(styles));
+    initLayouts(replaceableComponents);
   };
 }
 
-function initLayouts(store: Store) {
-  store.dispatch([
-    new AddReplaceableComponent({
-      key: eThemeBasicComponents.ApplicationLayout,
-      component: LayoutDefaultComponent,
-    }),
-    new AddReplaceableComponent({
-      key: eThemeBasicComponents.AccountLayout,
-      component: LayoutPassportComponent,
-    }),
-    new AddReplaceableComponent({
-      key: eThemeBasicComponents.EmptyLayout,
-      component: LayoutFullScreenComponent,
-    }),
-    new AddReplaceableComponent({
-      key: eThemeCoreComponents.PageBar,
-      component: PageBarComponent,
-    }),
-  ]);
+function initLayouts(replaceableComponents: ReplaceableComponentsService) {
+  replaceableComponents.add({
+    key: eThemeBasicComponents.ApplicationLayout,
+    component: LayoutDefaultComponent
+  });
+  replaceableComponents.add({
+    key: eThemeBasicComponents.AccountLayout,
+    component: LayoutPassportComponent
+  });
+  replaceableComponents.add({
+    key: eThemeBasicComponents.EmptyLayout,
+    component: LayoutFullScreenComponent
+  });
+  replaceableComponents.add({
+    key: eThemeCoreComponents.PageBar,
+    component: PageBarComponent
+  });
 }
--- libs/tmpl/theme-ng-alain/src/lib/index.ts
+++ libs/tmpl/theme-ng-alain/src/lib/index.ts
@@ -1,6 +1,5 @@
 export * from './ng-alain.module';
 export * from './module-import-guard';
-export * from './global-config.module';
 
 
 
