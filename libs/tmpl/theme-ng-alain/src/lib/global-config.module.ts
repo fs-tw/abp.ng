e828fe22ab5ac2bcd71cd07f2b90d0f1646a03ac
YinChang0626 <yinchang0626@gmail.com>
2020-10-03 12:19:36 +0800

    split root module

--- libs/tmpl/theme-ng-alain/src/lib/global-config.module.ts
+++ libs/tmpl/theme-ng-alain/src/lib/global-config.module.ts
@@ -1,32 +1,11 @@
 import { ModuleWithProviders, NgModule, Optional, SkipSelf } from '@angular/core';
 import { throwIfAlreadyLoaded } from '@fs/theme.ng-alain/core';
-import { DelonMockModule } from '@delon/mock';
 import { AlainThemeModule } from '@delon/theme';
 import { AlainConfig, ALAIN_CONFIG } from '@delon/util';
+import { NzConfig, NZ_CONFIG } from 'ng-zorro-antd/core/config';
 
 // Please refer to: https://ng-alain.com/docs/global-config
-// #region NG-ALAIN Config
-
-import { DelonACLModule } from '@delon/acl';
-
-const alainConfig: AlainConfig = {
-  st: { modal: { size: 'lg' } },
-  pageHeader: { homeI18n: 'home' },
-  lodop: {
-    license: `A59B099A586B3851E0F0D7FDBF37B603`,
-    licenseA: `C94CEE276DB2187AE6B65D56B3FC2848`,
-  },
-  auth: { login_url: '/passport/login' },
-};
-
-const alainModules = [AlainThemeModule.forRoot(), DelonACLModule.forRoot(), DelonMockModule.forRoot()];
-const alainProvides = [{ provide: ALAIN_CONFIG, useValue: alainConfig }];
-
-// mock
-import * as MOCKDATA from '../_mock';
-if (true) {
-  alainConfig.mock = { data: MOCKDATA };
-}
+// Please refer to: https://ng.ant.design/docs/global-config/en#how-to-use
 
 // #region reuse-tab
 /**
@@ -51,21 +30,20 @@ if (true) {
 
 // #endregion
 
-// #endregion
-
-// Please refer to: https://ng.ant.design/docs/global-config/en#how-to-use
-// #region NG-ZORRO Config
-
-import { NzConfig, NZ_CONFIG } from 'ng-zorro-antd/core/config';
-
+const alainConfig: AlainConfig = {
+  st: { modal: { size: 'lg' } },
+  pageHeader: { homeI18n: 'home' },
+  lodop: {
+    license: `A59B099A586B3851E0F0D7FDBF37B603`,
+    licenseA: `C94CEE276DB2187AE6B65D56B3FC2848`,
+  },
+  auth: { login_url: '/passport/login' },
+};
 const ngZorroConfig: NzConfig = {};
-
-const zorroProvides = [{ provide: NZ_CONFIG, useValue: ngZorroConfig }];
-
-// #endregion
-
 @NgModule({
-  imports: [...alainModules],
+  imports: [
+    AlainThemeModule.forRoot(),
+  ],
 })
 export class GlobalConfigModule {
   constructor(@Optional() @SkipSelf() parentModule: GlobalConfigModule) {
@@ -75,7 +53,10 @@ export class GlobalConfigModule {
   static forRoot(): ModuleWithProviders<GlobalConfigModule> {
     return {
       ngModule: GlobalConfigModule,
-      providers: [...alainProvides, ...zorroProvides],
+      providers: [
+        { provide: ALAIN_CONFIG, useValue: alainConfig },
+        { provide: NZ_CONFIG, useValue: ngZorroConfig }
+      ],
     };
   }
 }
--- libs/tmpl/theme-ng-alain/src/lib/ng-alain.module.ts
+++ libs/tmpl/theme-ng-alain/src/lib/ng-alain.module.ts
@@ -28,57 +28,11 @@ const LANG_PROVIDES = [
 
 
 import { CoreModule } from '@fs/theme.ng-alain/core';
-//import { STWidgetModule } from '@fs/theme.ng-alain/shared';
-
-// #endregion
-
-// #region root module
-import { GlobalConfigModule } from './global-config.module';
-import { NgAlainSharedModule } from '@fs/theme.ng-alain/shared';
-import { NgAlainBasicModule } from '@fs/theme.ng-alain/basic';
-import { NgxValidateCoreModule } from '@ngx-validate/core';
-import { ValidationErrorComponent } from '@fs/theme.ng-alain/basic';
-import { AlainConfigService } from '@delon/util';
-
-@NgModule({
-  imports: [
-    NgxValidateCoreModule.forRoot({
-      //targetSelector: 'nz-input-group',
-      blueprints: {
-        creditCard: 'AbpValidation::ThisFieldIsNotAValidCreditCardNumber.',
-        email: 'AbpValidation::ThisFieldIsNotAValidEmailAddress.',
-        invalid: 'AbpValidation::ThisFieldIsNotValid.',
-        max: 'AbpValidation::ThisFieldMustBeBetween{0}And{1}[{{ min }},{{ max }}]',
-        maxlength:
-          'AbpValidation::ThisFieldMustBeAStringOrArrayTypeWithAMaximumLengthOf{0}[{{ requiredLength }}]',
-        min: 'AbpValidation::ThisFieldMustBeBetween{0}And{1}[{{ min }},{{ max }}]',
-        minlength:
-          'AbpValidation::ThisFieldMustBeAStringOrArrayTypeWithAMinimumLengthOf{0}[{{ requiredLength }}]',
-        ngbDate: 'AbpValidation::ThisFieldIsNotValid.',
-        passwordMismatch: 'AbpIdentity::Identity.PasswordConfirmationFailed',
-        range: 'AbpValidation::ThisFieldMustBeBetween{0}And{1}[{{ min }},{{ max }}]',
-        required: 'AbpValidation::ThisFieldIsRequired.',
-        url: 'AbpValidation::ThisFieldIsNotAValidFullyQualifiedHttpHttpsOrFtpUrl',
-      },
-      errorTemplate: ValidationErrorComponent,
-    }),
-    GlobalConfigModule.forRoot(),
-    NgAlainSharedModule.forRoot(),
-    NgAlainBasicModule.forRoot(),
-  ],
-  providers: [
-    AlainConfigService,//Issue: https://github.com/ng-alain/ng-alain/issues/1624
-  ],  
-})
-export class RootModule { }
-
-// #endregion
-
+import { RootModule } from './root-module';
 
 @NgModule({
   imports: [
     CoreModule,
-    //STWidgetModule,
     RootModule
   ],
   providers: [
