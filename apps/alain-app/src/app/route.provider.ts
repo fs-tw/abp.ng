18ff03d4b77ba57ac5a3b62d18745f7abec2b426
ChangYinShung <changyenshung@gmail.com>
2021-04-28 00:42:41 +0800

    patch account router in order to use account-layout

--- apps/alain-app/src/app/route.provider.ts
+++ apps/alain-app/src/app/route.provider.ts
@@ -1,12 +1,16 @@
 import { RoutesService, eLayoutType } from '@abp/ng.core';
 import { APP_INITIALIZER } from '@angular/core';
 
+
 export const APP_ROUTE_PROVIDER = [
   { provide: APP_INITIALIZER, useFactory: configureRoutes, deps: [RoutesService], multi: true },
 ];
 
 function configureRoutes(routesService: RoutesService) {
   return () => {
+    routesService.patch('AbpAccount::Login',{
+      layout:eLayoutType.account
+    })
     routesService.add([
       {
         path: '/',
