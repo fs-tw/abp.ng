a3cbd1b1b20e45aadd6678e0cbf9c2f07720df77
yinchang0626 <yinchang0626@gmail.com>
2021-05-18 12:55:05 +0800

    remove G2 and PaheHeaderModule from shared of theme-alain

--- shared/src/lib/shared-delon.module.ts
+++ shared/src/lib/shared-delon.module.ts
@@ -1,7 +1,5 @@
-import { PageHeaderModule } from '@delon/abc/page-header';
 import { SEModule } from '@delon/abc/se';
 import { STModule } from '@delon/abc/st';
 import { SVModule } from '@delon/abc/sv';
-import { G2CustomModule } from '@delon/chart/custom';
 
-export const SHARED_DELON_MODULES = [STModule, SVModule, SEModule, PageHeaderModule, G2CustomModule];
+export const SHARED_DELON_MODULES = [STModule, SVModule, SEModule];
