ac80c30054ea827a5d3eab1288936c8606849b49
YinChang <yinchang0626@gmail.com>
2024-07-15 15:11:26 +0800

    update package version

--- libs/schematics/src/generators/package/lib/versions.ts
+++ libs/schematics/src/generators/package/lib/versions.ts
@@ -1,5 +1,5 @@
-export const angularVersion = '~17.3.6';
-export const abpVersion = '~8.1.3';
-export const leptonXVersion = '~3.1.3';
+export const angularVersion = '~17.3.11';
+export const abpVersion = '~8.2.0';
+export const leptonXVersion = '~3.2.0';
 export const bootstrapIconsVersion = '~1.8.3';
 export const ngrxVersion = "^17.2.0";
